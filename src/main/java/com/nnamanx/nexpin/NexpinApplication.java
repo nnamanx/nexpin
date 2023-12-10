656b7d4080ba5ca4de5c537194647b9faf94dfe4
Laman Khudadatzada <lkhudadatzada@gmail.com>
2023-12-10 12:03:59 +0400

    "build gradle"

---
+++ src/main/java/com/nnamanx/nexpin/NexpinApplication.java
@@ -0,0 +1,13 @@
+package com.nnamanx.nexpin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexpinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexpinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nnamanx/nexpin/NexpinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nnamanx.nexpin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexpinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
