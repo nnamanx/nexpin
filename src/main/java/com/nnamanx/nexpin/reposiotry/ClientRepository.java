1e6c812bcdad432162d85c1749fd5ffc622d4bd3
Laman Khudadatzada <lkhudadatzada@gmail.com>
2023-12-25 21:26:20 +0400

    "honestly, idk"

--- src/main/java/com/nnamanx/nexpin/reposiotry/ClientRepository.java
+++ src/main/java/com/nnamanx/nexpin/reposiotry/ClientRepository.java
@@ -3,5 +3,9 @@ package com.nnamanx.nexpin.reposiotry;
 import com.nnamanx.nexpin.model.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClientRepository extends JpaRepository<Client, Long> {
+
+    Optional<Client> findByUsername(String username); // The username might not exist in the database, so that we use Optional not to return NULLPOINTEREXCEPTION
 }
