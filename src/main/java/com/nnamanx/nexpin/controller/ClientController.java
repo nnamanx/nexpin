5658d03d659fd824899a05e230313cbeea28705f
Laman Khudadatzada <lkhudadatzada@gmail.com>
2024-01-11 19:11:09 +0400

    "client controller"

--- src/main/java/com/nnamanx/nexpin/controller/ClientController.java
+++ src/main/java/com/nnamanx/nexpin/controller/ClientController.java
@@ -1,16 +1,54 @@
 package com.nnamanx.nexpin.controller;
 
+import com.nnamanx.nexpin.model.entity.Client;
 import com.nnamanx.nexpin.service.ClientService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("/nexpin/client")
 @RequiredArgsConstructor
 public class ClientController {
 
     private final ClientService clientService;
+
+    @PatchMapping("/{clientId}")
+    public ResponseEntity<Client> updateClientParameter(
+            @PathVariable Long clientId,
+            @RequestParam String fieldName,
+            @RequestParam Object updatedValue
+    ) {
+
+        Client updatedClient = clientService.updateClientParameter(clientId, fieldName, updatedValue);
+        return ResponseEntity.ok(updatedClient);
+    }
+
+    @GetMapping("/{clientId}")
+    public ResponseEntity<Client> getClientById(@PathVariable Long clientId) {
+
+        Client client = clientService.getClientById(clientId);
+        return ResponseEntity.ok(client);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Client>> getAllClients() {
+
+        List<Client> clients = clientService.getAllClients();
+        return ResponseEntity.ok(clients);
+    }
+
+    @PostMapping("/{clientId}/change-password")
+    public ResponseEntity<Client> changePassword(
+            @PathVariable Long clientId,
+            @RequestParam String newPassword
+    ) {
+
+        Client updatedClient = clientService.changePassword(clientId, newPassword);
+        return ResponseEntity.ok(updatedClient);
+    }
 
 
 }
