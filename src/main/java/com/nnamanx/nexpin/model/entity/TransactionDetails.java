52d2d6de1b0974473bb7bd1c168a3cd0f41d5c3e
Laman Khudadatzada <lkhudadatzada@gmail.com>
2024-01-12 21:08:11 +0400

    "changing port"

--- src/main/java/com/nnamanx/nexpin/model/entity/TransactionDetails.java
+++ src/main/java/com/nnamanx/nexpin/model/entity/TransactionDetails.java
@@ -17,7 +17,7 @@ public class TransactionDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
-    String organization_service_code; //azerisiq abonent kodu, bazada saxlanilmir;
+    String organization_service_code; //azerisiq abonent kodu (bazada saxlanilmir)
 
     @ManyToOne
     @JoinColumn(name = "transaction_id")
