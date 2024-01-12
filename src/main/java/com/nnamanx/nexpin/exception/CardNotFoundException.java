4b89fc7e3ae239e7fc8a3e65f907727de12f6cb0
Laman Khudadatzada <lkhudadatzada@gmail.com>
2024-01-12 20:08:08 +0400

    "card not found exception "

---
+++ src/main/java/com/nnamanx/nexpin/exception/CardNotFoundException.java
@@ -0,0 +1,8 @@
+package com.nnamanx.nexpin.exception;
+
+public class CardNotFoundException extends RuntimeException {
+
+    public CardNotFoundException(String message) {
+        super(message);
+    }
+}
