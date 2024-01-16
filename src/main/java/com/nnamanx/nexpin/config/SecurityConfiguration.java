fe3f810e2ff51ba348a7de939314456bbe1bc257
Laman Khudadatzada <lkhudadatzada@gmail.com>
2024-01-16 22:44:44 +0400

    "Security Configuration"

---
+++ src/main/java/com/nnamanx/nexpin/config/SecurityConfiguration.java
@@ -0,0 +1,42 @@
+package com.nnamanx.nexpin.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.context.NullSecurityContextRepository;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeRequests(authorize -> authorize
+                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/nexpin/client/**").permitAll()
+                        .requestMatchers("/nexpin/account/**").permitAll()
+                        .requestMatchers("/nexpin/transaction/**").permitAll()
+                        .requestMatchers("/nexpin/services/**").permitAll()
+                        .requestMatchers("/nexpin/cashback/**").permitAll()
+                        .anyRequest().authenticated())
+                .securityContext(context ->
+                        context.securityContextRepository(new NullSecurityContextRepository()))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
